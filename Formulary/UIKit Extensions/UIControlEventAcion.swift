ff03641009e2fc9fbd6812205597091d98b407bf
luckychris <lucky_chris_again@yahoo.de>
2019-05-05 22:23:11 +0200

    Update UIControlEventAcion.swift

--- Formulary/UIKit Extensions/UIControlEventAcion.swift
+++ Formulary/UIKit Extensions/UIControlEventAcion.swift
@@ -10,12 +10,12 @@ import UIKit
 
 private var ActionTargetControlKey = 0
 
-func bind(_ control: UIControl, controlEvents: UIControlEvents = .valueChanged, action: @escaping (UIControl) -> Void) {
+func bind(_ control: UIControl, controlEvents: UIControl.Event = .valueChanged, action: @escaping (UIControl) -> Void) {
     let actionTarget = ActionTarget(control: control, action: action, controlEvents: controlEvents)
     objc_setAssociatedObject(control, &ActionTargetControlKey, actionTarget, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
 }
 
-func clear(_ control :UIControl, controlEvents :UIControlEvents) {
+func clear(_ control :UIControl, controlEvents :UIControl.Event) {
     if let target = objc_getAssociatedObject(control, &ActionTargetControlKey) as? ActionTarget {
         control.removeTarget(target, action: #selector(ActionTarget.action(_:)), for: controlEvents)
         objc_setAssociatedObject(control, &ActionTargetControlKey, nil, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
@@ -26,7 +26,7 @@ func clear(_ control :UIControl, controlEvents :UIControlEvents) {
 private class ActionTarget {
     weak var control: UIControl?
     let closure: (UIControl) -> Void
-    init(control: UIControl, action: @escaping (UIControl) -> Void, controlEvents: UIControlEvents) {
+    init(control: UIControl, action: @escaping (UIControl) -> Void, controlEvents: UIControl.Event) {
         self.control = control
         closure = action
         control.addTarget(self, action: #selector(ActionTarget.action(_:)), for: controlEvents)
